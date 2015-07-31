76936e2cb2dca5d17a361cc26471e6c0f9e4d4c8
jin0902 <jjjin0902@gmail.com>
2015-07-31 11:52:32 +0900

    Create Check Nums.rb

---
+++ Check Nums.rb
@@ -0,0 +1,12 @@
+def CheckNums(num1,num2)
+
+   if     (num2 > num1)
+           true
+   elsif  (num2 < num1)
+           false
+   else   (num2 == num1)
+           -1
+   end
+   
+         
+end
